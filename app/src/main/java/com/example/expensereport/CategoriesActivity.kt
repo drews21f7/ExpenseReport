ad4b46b19400cb756afe1d3f0cc3522d90734bf8
Drew Seeholzer <drew.seeholzer@drewslaptop.local>
2022-10-03 15:10:23 -0600

    Added functions to CategoriesRepo, and testdatabase in CategoriesActivity.

--- app/src/main/java/com/example/expensereport/CategoriesActivity.kt
+++ app/src/main/java/com/example/expensereport/CategoriesActivity.kt
@@ -3,6 +3,8 @@ package com.example.expensereport
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
+import com.example.expensereport.Database.CategoryDatabase
+import com.example.expensereport.Database.entities.CategoryEntity
 
 class CategoriesActivity : AppCompatActivity() {
     private lateinit var categoriesRecycler: RecyclerView
@@ -15,6 +17,20 @@ class CategoriesActivity : AppCompatActivity() {
         categoryRepo.categoryList.addAll(FakeDatabase.getSavedCategories())
         val adaptor = CategoryAdaptor(categoryRepo.categoryList)
         categoriesRecycler.adapter = adaptor
+        testDatabase()
+    }
+
+    fun testDatabase () {
+        var database = CategoryDatabase.getDatabase(this)
+        var category = CategoryEntity(title = "Billtest", totalPrice = 10.01)
+        var category2 = CategoryEntity(title = "Billtest2", totalPrice = 10.02)
+        var category3 = CategoryEntity(title = "Billtest3", totalPrice = 10.03)
+        var category4 = CategoryEntity(title = "Billtest4", totalPrice = 10.04)
+
+        database.categoryDao().insert(category)
+        database.categoryDao().insert(category2)
+        database.categoryDao().insert(category3)
+        database.categoryDao().insert(category4)
     }
 
 }
--- app/src/main/java/com/example/expensereport/CategoriesRepo.kt
+++ app/src/main/java/com/example/expensereport/CategoriesRepo.kt
@@ -1,5 +1,38 @@
 package com.example.expensereport
 
-class CategoriesRepo {
-    var categoryList: ArrayList<Category> = arrayListOf()
+import android.content.Context
+import com.example.expensereport.Database.CategoryDatabase
+import com.example.expensereport.Database.entities.CategoryEntity
+
+class CategoriesRepo(context: Context) {
+    private val categoryList: ArrayList<Category> = arrayListOf()
+    private val database: CategoryDatabase
+
+    init {
+        database = CategoryDatabase.getDatabase(context)
+        initializeCategoryList()
+    }
+
+    fun addCategory(category: Category) {
+        database.categoryDao().insert(category.toEntity())
+    }
+
+    fun getAllCategories(): ArrayList<Category> {
+        return categoryList
+    }
+
+    private fun initializeCategoryList() {
+        var categories = database.categoryDao().getAll()
+        categories.forEach{
+            categoryList.add(it.toCategory())
+        }
+    }
+}
+
+fun Category.toEntity(): CategoryEntity {
+    return CategoryEntity(title = title, totalPrice = totalPrice)
+}
+
+fun CategoryEntity.toCategory(): Category {
+    return Category(title = title, totalPrice = totalPrice)
 }
